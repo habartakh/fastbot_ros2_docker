1d0abb270a5e8bd5d102192360bf859364a54568
habartakh <bartakh.hajar@gmail.com>
2025-08-03 20:42:43 +0000

    Modified fastbot real image

--- real/docker/volumes/ros_entrypoint.sh
+++ real/docker/volumes/ros_entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI=/var/lib/theconstruct.rrl/cyclonedds.xml
 # setup ros2 environment
-source "/opt/ros/$ROS_DISTRO/setup.bash" --
+source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/root/ros2_ws/install/setup.bash"
 exec "$@"
