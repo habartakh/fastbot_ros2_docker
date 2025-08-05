106096d0ba987a5afc265099dc49c531b9a5d3c3
habartakh <bartakh.hajar@gmail.com>
2025-08-05 19:02:49 +0000

    Adjusted webpage layout to fit instructions

--- simulation/entrypoint.sh
+++ simulation/entrypoint.sh
@@ -6,11 +6,5 @@ source /opt/ros/humble/setup.bash
 source /ros2_ws/install/setup.bash
 
 echo "$(date +'[%Y-%m-%d %T]') Starting webpage container..."
-python3 -m http.server 7000 & ros2 launch rosbridge_server rosbridge_websocket_launch.xml 
+python3 -m http.server 7000 & ros2 launch rosbridge_server rosbridge_websocket_launch.xml
 
-# # Run rosbridge
-# echo "$(date +'[%Y-%m-%d %T]') Launching rosbridge..."
-# ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
-
-# # Optional: keep container alive
-# tail -f /dev/null
