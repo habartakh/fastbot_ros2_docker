e6985d48cc61ccae08e01be3aef8f032b81d2a8c
habartakh <bartakh.hajar@gmail.com>
2025-08-05 15:55:17 +0000

    Solved communication bug between webpage and ROS2 nodes

--- simulation/entrypoint.sh
+++ simulation/entrypoint.sh
@@ -1,15 +1,16 @@
 #! /bin/bash 
+set -e
 
-echo "$(date +'[%Y-%m-%d %T]') Starting nginx server..."
-# set -x
-# nginx -g 'daemon off;'
+# Source ROS 2 else ros2 command is not recognized
+source /opt/ros/humble/setup.bash
+source /ros2_ws/install/setup.bash
 
-# Run nginx in background
-nginx 
+echo "$(date +'[%Y-%m-%d %T]') Starting webpage container..."
+python3 -m http.server 7000 & ros2 launch rosbridge_server rosbridge_websocket_launch.xml 
 
-# Run rosbridge
-echo "$(date +'[%Y-%m-%d %T]') Launching rosbridge..."
-ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
+# # Run rosbridge
+# echo "$(date +'[%Y-%m-%d %T]') Launching rosbridge..."
+# ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
 
-# Optional: keep container alive
-tail -f /dev/null
+# # Optional: keep container alive
+# tail -f /dev/null
